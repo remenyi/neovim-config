adbcec82cff9789b97c8dfe41de83023dab1ee89
Reményi Gergely <remenyig@gmail.com>
2024-04-23 21:58:54 +0200

    Remove git tree

--- after/plugin/tree.lua
+++
@@ -1,5 +0,0 @@
-local tree = require("nvim-tree")
-
-tree.setup()
-
-vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
--- init.lua
+++ init.lua
@@ -2,9 +2,6 @@ vim.g.mapleader = " "
 
 vim.g.have_nerd_font = true
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 vim.o.clipboard = "unnamedplus"
 
 vim.o.showmode = false
@@ -124,10 +121,6 @@ require("lazy").setup({
     { 'rcarriga/nvim-notify' },
     { 'github/copilot.vim' },
     { 'ahmedkhalf/project.nvim' },
-    {
-        'nvim-tree/nvim-tree.lua',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-    },
     { 'ThePrimeagen/git-worktree.nvim' },
     {
         "iabdelkareem/csharp.nvim",
