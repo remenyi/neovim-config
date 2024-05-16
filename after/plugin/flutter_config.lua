4cbced666ed1c37491ece0230e4c34d3d3790d23
Gergely Reményi <remenyig@gmail.com>
2024-05-16 18:26:02 +0200

    Remove nvim_cmp from flutter_tools (much faster now!)

--- after/plugin/flutter_config.lua
+++ after/plugin/flutter_config.lua
@@ -1,14 +1,11 @@
 local flutter_tools = require("flutter-tools")
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
 flutter_tools.setup {
     ui = {
         border = "rounded",
     },
     fvm = true,
     lsp = {
-        capabilities = capabilities,
         settings = {
             lineLength = 120,
         },
